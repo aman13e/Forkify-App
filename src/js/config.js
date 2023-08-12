34d27a4017b42c505a3f7149e713899e3151b07a
aman13e <amanuelaweke13@gmail.com>
2023-08-12 11:54:31 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '969e88fb-4cc3-43f0-9e2b-e2167d5849a1';
+// export const KEY = '<YOUR_KEY>';
+export const MODAL_CLOSE_SEC = 2.5;
+
+// export const KEY = '';
